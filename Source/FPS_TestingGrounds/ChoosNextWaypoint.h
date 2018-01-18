d67740bb265d0d6d2463643d1542436c7c14a47d
MSI\USER <s.hidalgo@xenocreatives.com>
2018-01-18 22:49:36 +0800

    Started C++ for AI

---
+++ Source/FPS_TestingGrounds/ChoosNextWaypoint.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChoosNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_TESTINGGROUNDS_API UChoosNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+	
+};
